32fa5dc9966887fa07aac863d883a0aa76e4eb89
Luciano Quaranta <Luciano_40@hotmail.it>
2014-03-24 09:27:59 +0100

    aggiunto secondo commento

---
+++ src/it/uniroma3/dia/db1/jdbc/persistence/DataSource.java
@@ -0,0 +1,23 @@
+package it.uniroma3.dia.db1.jdbc.persistence;
+
+import java.sql.*;
+
+public class DataSource {
+	private String dbURI = "jdbc:postgresql://localhost:5431/university";
+	private String user = "postgres";
+	private String password = "siw";
+	//modificare la porta se se ne usa un'altra
+	//commento 2
+
+	public Connection getConnection()  {
+		Connection connection = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(dbURI,user, password);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
